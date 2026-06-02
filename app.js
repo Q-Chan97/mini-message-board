e532fffd824dae3843a6e9ab6e0a6ef27f8bed0e
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-06-02 12:13:20 -0500

    feat: Setup server and index path

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import indexRouter from "./routers/indexRouter.js";
+
+import path from "node:path";
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+const __dirname = import.meta.dirname;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+
+app.listen(PORT, (error) => {
+    if (error) throw error;
+    console.log(`Server listening on port ${PORT}`);
+})
