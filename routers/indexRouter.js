d1725c7e059fb2941538c40acdcf5478335c9829
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-06-02 13:35:53 -0500

    feat: Pass title and messages to index view

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -2,8 +2,21 @@ import Router from "express";
 
 const indexRouter = Router();
 
+const messages = [
+    {
+        text: "Hey there",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "Hello world!",
+        user: "Charles",
+        added: new Date(),
+    },
+]
+
 indexRouter.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", {title: "Mini Message Board", messages: messages});
 });
 
 export default indexRouter;
