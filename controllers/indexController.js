2c8f30eef93fa54a04e5042dfe8993d08bc04c00
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-06-03 12:05:29 -0500

    feat: Create getMessageById

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,7 @@
 import { format } from "date-fns";
 
 export function formatDate() {
-    const timestamp = format(new Date(), "h:mmbbb M/dd/yy ");
+    const timestamp = format(new Date(), "h:mmaaa M/dd/yy ");
 
     return timestamp;
 }
@@ -10,4 +10,16 @@ let currentId = 0;
 
 export function createId() {
     return ++currentId;
+}
+
+export function getMessageById(messages) {
+    return (req, res) => {
+        const { messageId } = req.params;
+
+        const message = messages.find((message) => {
+            return message.id === Number(messageId);
+        });
+
+        res.render("details", { message })
+    }
 }
