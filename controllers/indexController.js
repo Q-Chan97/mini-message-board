2ebe47699f851754b8182236650cecdd6583a762
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-06-04 23:22:51 -0500

    feat: Implement form validation

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,10 +1,19 @@
 import { RegExpMatcher, TextCensor, englishDataset, englishRecommendedTransformers } from "obscenity";
+import { body, validationResult, matchedData } from "express-validator";
+
 
 const matcher = new RegExpMatcher({
     ...englishDataset.build(),
     ...englishRecommendedTransformers,
 });
 const censor = new TextCensor();
+
+const validateMessage = [
+    body("nameText").trim()
+        .isLength({min: 1, max: 18}).withMessage("Name must be between 1 and 18 characters"),
+    body("messageText").trim()
+        .isLength({min: 1, max: 280}).withMessage("Message must be between 1 and 280 characters"),
+]
 
 let currentId = 0;
 
@@ -24,8 +33,29 @@ export function getMessageById(messages) {
     }
 }
 
-export function checkProfanity(text) {
+function checkProfanity(text) {
     const matches = matcher.getAllMatches(text);
     const filteredText = censor.applyTo(text, matches);
     return filteredText;
+}
+
+export function messageCreatePost(messages) {
+    return [
+    validateMessage,
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).render("form", {
+                errors: errors.array(),
+            });
+        }
+        const { messageText, nameText } = matchedData(req);
+
+        const filteredMessage = checkProfanity(messageText);
+        const filteredName = checkProfanity(nameText);
+
+        messages.push({ id: createId(), text: filteredMessage, user: filteredName, added: new Date() });
+        res.redirect("/");
+    },
+    ]
 }
