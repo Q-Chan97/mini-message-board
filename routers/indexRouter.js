35b818f1e3362ed9c07c0418a2d59b69e741a15a
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-06-08 17:54:51 -0500

    feat: Update w/ new controllers

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,29 +1,44 @@
 import Router from "express";
-import { createId, getMessageById, messageCreatePost } from "../controllers/indexController.js";
+import { displayUserMessages, getMessageById, messageCreatePost } from "../controllers/indexController.js";
 
 const indexRouter = Router();
 
-const messages = [
-    {
-        id: createId(),
-        text: "Hey there",
-        user: "Amando",
-        added: new Date(),
-    },
-    {
-        id: createId(),
-        text: "Hello world!",
-        user: "Charles",
-        added: new Date(),
-    },
-]
+// PostgreSQL Routes
 
-indexRouter.get("/", (req, res) => {
-    res.render("index", {messages: messages});
-});
+indexRouter.get("/", displayUserMessages);
 
-indexRouter.post("/new", messageCreatePost(messages));
+indexRouter.get("/new", (req, res) => {
+    res.render("form");
+})
+indexRouter.post("/new", ...messageCreatePost());
 
-indexRouter.get("/details/:messageId", getMessageById(messages));
+indexRouter.get("/details/:messageId", getMessageById);
+
+
+
+// Hard-Coded Database Routes
+
+// const messages = [
+//     {
+//         id: createId(),
+//         text: "Hey there",
+//         user: "Amando",
+//         added: new Date(),
+//     },
+//     {
+//         id: createId(),
+//         text: "Hello world!",
+//         user: "Charles",
+//         added: new Date(),
+//     },
+// ]
+
+// indexRouter.get("/", (req, res) => {
+//     res.render("index", {messages: messages});
+// });
+
+// indexRouter.post("/new", messageCreatePost(messages));
+
+// indexRouter.get("/details/:messageId", getMessageById(messages));
 
 export default indexRouter;
